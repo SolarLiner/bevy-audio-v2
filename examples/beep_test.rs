1aca12e654e0eedfe4f2f81d699ee258485644c4
Nathan Graule <nathan.graule@arturia.com>
2024-10-16 17:16:11 +0200

    wip: beep test example

--- examples/beep_test.rs
+++ examples/beep_test.rs
@@ -1,8 +1,141 @@
+use atomic_float::AtomicF32;
 use bevy::prelude::*;
-use bevy_audio_v2::AudioPlugin;
+use bevy_audio_v2::{AudioPlugin, NodeId, UpdateAudioGraphExt};
+use firewheel::graph::AudioGraph;
+use firewheel::node::{AudioNode, AudioNodeInfo, AudioNodeProcessor, ProcInfo};
+use firewheel::BlockFrames;
+use std::error::Error;
+use std::f32::consts::TAU;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+
+struct BeepPlugin;
+
+impl Plugin for BeepPlugin {
+    fn build(&self, app: &mut App) {
+        app.observe(on_add_beep).observe(on_remove_beep);
+    }
+}
+
+fn on_add_beep(trigger: Trigger<OnAdd, Beep>, mut commands: Commands) {
+    commands.entity(trigger.entity()).update_audio_graph(
+        |mut entity: EntityWorldMut, audio_graph: &mut AudioGraph<(), 512>| {
+            let beep = entity.get::<Beep>().unwrap();
+            let node: Box<dyn AudioNode<_, 512>> = Box::new(BeepNode(Arc::new(BeepNodeImpl {
+                amplitude: AtomicF32::new(beep.amplitude),
+                frequency: AtomicF32::new(beep.frequency),
+            })));
+            let node = audio_graph.add_node(0, 1, node);
+            audio_graph
+                .connect(node, 0, audio_graph.graph_out_node(), 0, false)
+                .unwrap();
+            audio_graph
+                .connect(node, 0, audio_graph.graph_out_node(), 1, false)
+                .unwrap();
+            entity.insert(NodeId(node));
+        },
+    );
+}
+
+fn on_remove_beep(trigger: Trigger<OnRemove, Beep>, mut commands: Commands) {
+    commands.entity(trigger.entity()).update_audio_graph(
+        |mut entity, audio_graph| {
+            let NodeId(node_id) = *entity.get::<NodeId>().unwrap();
+            audio_graph.remove_node(node_id).unwrap();
+            entity.remove::<Beep>();
+        },
+    )
+}
+
+#[derive(Debug)]
+struct BeepNodeImpl {
+    amplitude: AtomicF32,
+    frequency: AtomicF32,
+}
+
+#[derive(Debug, Clone, Deref)]
+struct BeepNode(Arc<BeepNodeImpl>);
+
+impl<C, const MBF: usize> AudioNode<C, MBF> for BeepNode {
+    fn debug_name(&self) -> &'static str {
+        "beep"
+    }
+
+    fn info(&self) -> AudioNodeInfo {
+        AudioNodeInfo {
+            num_min_supported_inputs: 0,
+            num_max_supported_inputs: 0,
+            num_min_supported_outputs: 1,
+            num_max_supported_outputs: 1,
+        }
+    }
+
+    fn activate(
+        &mut self,
+        sample_rate: u32,
+        num_inputs: usize,
+        num_outputs: usize,
+    ) -> Result<Box<dyn AudioNodeProcessor<C, MBF>>, Box<dyn Error>> {
+        Ok(Box::new(BeepNodeProcessor {
+            params: self.clone(),
+            discretization_factor: TAU / sample_rate as f32,
+            phase: 0.,
+        }))
+    }
+}
+
+struct BeepNodeProcessor {
+    params: BeepNode,
+    discretization_factor: f32,
+    phase: f32,
+}
+
+impl<C, const MBF: usize> AudioNodeProcessor<C, MBF> for BeepNodeProcessor {
+    fn process(
+        &mut self,
+        frames: BlockFrames<MBF>,
+        inputs: &[&[f32; MBF]],
+        outputs: &mut [&mut [f32; MBF]],
+        proc_info: ProcInfo<C>,
+    ) {
+        let step = self.params.frequency.load(Ordering::Relaxed) * self.discretization_factor;
+        let amplitude = self.params.amplitude.load(Ordering::Relaxed);
+        for i in 0..frames.get() {
+            outputs[0][i] = self.phase.sin() * amplitude;
+            self.phase += step;
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Component)]
+struct Beep {
+    amplitude: f32,
+    frequency: f32,
+}
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, AudioPlugin))
+        .add_plugins((DefaultPlugins, AudioPlugin, BeepPlugin))
+        .add_systems(Update, toggle_beep)
         .run();
+}
+
+fn toggle_beep(
+    mut current_entity: Local<Option<Entity>>,
+    mut commands: Commands,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        if let Some(entity) = *current_entity {
+            commands.entity(entity).despawn();
+        } else {
+            let entity = commands
+                .spawn(Beep {
+                    frequency: 440.,
+                    amplitude: 0.125,
+                })
+                .id();
+            current_entity.replace(entity);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use bevy_utils::synccell::SyncCell;
 use bevy_app::prelude::*;
+use bevy_derive::{Deref, DerefMut};
 use bevy_ecs::prelude::*;
+use bevy_ecs::system::{EntityCommand, EntityCommands};
+use bevy_ecs::world::Command;
 use bevy_log as log;
+use firewheel::graph::{AudioGraph, NodeID};
 use firewheel::{ActiveCtx, InactiveCtx, UpdateStatus};
 use std::ops;
 
@@ -36,16 +39,25 @@ pub struct InputDevice(pub String);
 #[derive(Debug, Clone, Resource)]
 pub struct OutputDevice(pub String);
 
+#[derive(Debug, Copy, Clone, Deref, DerefMut, Component)]
+pub struct NodeId(pub NodeID);
+
 pub struct AudioPlugin;
 
 impl Plugin for AudioPlugin {
     fn build(&self, app: &mut App) {
         app.init_non_send_resource::<AudioEngineBuilder>();
-        app.add_systems(Last, update_audio_engine).add_systems(PostUpdate, update_output_device.run_if(resource_exists_and_changed::<OutputDevice>));
+        app.add_systems(Last, update_audio_engine).add_systems(
+            PostUpdate,
+            update_output_device.run_if(resource_exists_and_changed::<OutputDevice>),
+        );
     }
 
     fn finish(&self, app: &mut App) {
-        let AudioEngineBuilder(cx) = app.world_mut().remove_non_send_resource::<AudioEngineBuilder>().unwrap();
+        let AudioEngineBuilder(cx) = app
+            .world_mut()
+            .remove_non_send_resource::<AudioEngineBuilder>()
+            .unwrap();
         // let input_device = app.world().get_resource::<InputDevice>().map(|s| &s.0);
         let output_device = app.world().get_resource::<OutputDevice>().map(|s| &s.0);
         let cx = cx
@@ -56,7 +68,9 @@ impl Plugin for AudioPlugin {
 }
 
 fn update_output_device(world: &mut World) {
-    let AudioEngine(cx) = world.remove_non_send_resource().expect("Audio engine incorrectly set up");
+    let AudioEngine(cx) = world
+        .remove_non_send_resource()
+        .expect("Audio engine incorrectly set up");
     let OutputDevice(out_device) = world.resource();
     log::info!("Changing output device to {out_device:?}");
 
@@ -69,7 +83,9 @@ fn update_output_device(world: &mut World) {
 
 // Exclusive system because the audio engine requires moving itself out and back in
 fn update_audio_engine(world: &mut World) {
-    let AudioEngine(cx) = world.remove_non_send_resource().expect("Audio engine incorrectly set up");
+    let AudioEngine(cx) = world
+        .remove_non_send_resource()
+        .expect("Audio engine incorrectly set up");
     if !world.resource::<Events<AppExit>>().is_empty() {
         cx.deactivate();
         log::info!("Shutting down audio engine");
@@ -89,5 +105,65 @@ fn update_audio_engine(world: &mut World) {
                 log::error!("Audio engine error: {error}");
             }
         }
+    }
+}
+
+fn apply_audio_graph_command(
+    world: &mut World,
+    apply: impl FnOnce(&mut World, &mut AudioGraph<(), 512>),
+) {
+    let AudioEngine(cx) = world
+        .remove_non_send_resource::<AudioEngine>()
+        .expect("Audio engine incorrectly set up");
+    let out_device = cx.out_device_name().to_string();
+    let (mut cx, _) = cx.deactivate();
+    let audio_graph = cx.graph_mut();
+
+    apply(world, audio_graph);
+
+    world.insert_non_send_resource(AudioEngine(
+        cx.activate(Some(&out_device), true, ())
+            .expect("Cannot restart audio engine"),
+    ));
+}
+
+pub struct UpdateAudioGraphCommand<F>(F);
+
+impl<F: 'static + Send + FnOnce(&mut World, &mut AudioGraph<(), 512>)> Command
+    for UpdateAudioGraphCommand<F>
+{
+    fn apply(self, world: &mut World) {
+        apply_audio_graph_command(world, self.0);
+    }
+}
+
+impl<F: 'static + Send + FnOnce(Option<EntityWorldMut>, &mut AudioGraph<(), 512>)> EntityCommand
+    for UpdateAudioGraphCommand<F>
+{
+    fn apply(self, id: Entity, world: &mut World) {
+        apply_audio_graph_command(world, |world, audio_graph| {
+            let entity_world = world.get_entity_mut(id);
+            self.0(entity_world, audio_graph);
+        });
+    }
+}
+
+pub trait UpdateAudioGraphExt<F> {
+    fn update_audio_graph(&mut self, apply: F);
+}
+
+impl<'w, 's, F: 'static + Send + FnOnce(&mut World, &mut AudioGraph<(), 512>)>
+    UpdateAudioGraphExt<F> for Commands<'w, 's>
+{
+    fn update_audio_graph(&mut self, apply: F) {
+        self.add(UpdateAudioGraphCommand(apply));
+    }
+}
+
+impl<'a, F: 'static + Send + FnOnce(Option<EntityWorldMut>, &mut AudioGraph<(), 512>)>
+    UpdateAudioGraphExt<F> for EntityCommands<'a>
+{
+    fn update_audio_graph(&mut self, apply: F) {
+        self.add(UpdateAudioGraphCommand(apply));
     }
 }
